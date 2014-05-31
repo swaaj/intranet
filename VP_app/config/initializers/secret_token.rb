fc3cc0039ae39e629ad392d0d2da94d872bf37d8
eddyss <edseignol@yahoo.fr>
2014-05-31 16:08:45 +0200

    Création de l'appli VP

---
+++ VP_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VPApp::Application.config.secret_key_base = 'd2c7691f21ffc9d02b9ca1862d14d1977a7660ad087fd2b739633d1a35883c85bc66c4db28e78f335c0e077ca2e6c0522d0d18939018d1237e22c24c037a4438'
---
+++ VP_app/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VPApp::Application.config.session_store :cookie_store, key: '_VP_app_session'
